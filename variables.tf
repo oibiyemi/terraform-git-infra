813cb9686a5565eb3eece9909dd321fcb643742e
oibiyemi <shawn.active@yahoo.com>
2025-10-30 03:18:44 +0000

    feat: Configure Repositories with Repo files and Pages

--- variables.tf
+++ variables.tf
@@ -12,44 +12,28 @@
 # Helps keep all repos consistent and easy to manage.
 # -----------------------------------------------
 
-variable "repo_config" {
+variable "repo_configs" {
   description = "Configuration map for GitHub repositories"
   type = map(object({
     description        = string,
     visibility         = string,
     auto_init          = bool,
-    has_wiki           = bool
-    allow_merge_commit = bool
-    allow_squash_merge = bool
-    allow_rebase_merge = bool
+    has_wiki           = bool,
+    allow_merge_commit = bool,
+    allow_squash_merge = bool,
+    allow_rebase_merge = bool,
+    enable_pages       = bool,
+    repo_file = object({
+      branch = string,
+      file   = string,
+      content = object({
+        file_title = string,
+        repo_name  = string,
+        author     = string,
+      }),
+      commit_message      = string,
+      overwrite_on_create = bool,
+
+    })
   }))
-  default = {
-    core-api-service = {
-      description        = "The core microservice API for business logic.",
-      visibility         = "private",
-      auto_init          = true,
-      has_wiki           = false
-      allow_merge_commit = false
-      allow_squash_merge = true
-      allow_rebase_merge = true
-    },
-    docs-portal = {
-      description        = "Public documentation hub for API specifications.",
-      visibility         = "public",
-      auto_init          = true,
-      has_wiki           = true
-      allow_merge_commit = false
-      allow_squash_merge = true
-      allow_rebase_merge = true
-    },
-    asset-hub = {
-      description        = "Centralized repository for shared application assets.",
-      visibility         = "private",
-      auto_init          = true,
-      has_wiki           = false
-      allow_merge_commit = false
-      allow_squash_merge = true
-      allow_rebase_merge = true
-    }
-  }
 }
